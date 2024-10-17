92fb74dcdf2cfca50632c3f71748f0a13d46f98e
D3adSec <bruno.emedeiros99@gmail.com>
2024-10-17 11:49:32 -0300

    v1.0.0

--- src/app.js
+++ src/app.js
@@ -27,10 +27,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(fileUpload())
 
 const s3Client = new S3Client({
-    region: 'sa-east-1',
+    region: ,
     credentials: { 
-        accessKeyId: 'AKIA2HVQ5S572LOQGTXB',
-        secretAccessKey: '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'
+        accessKeyId: ,
+        secretAccessKey: 
     }
 });
 
